1c9ccae77c4dd633da0832e7b86c4aa7e0411276
u005 <47481735+u005@users.noreply.github.com>
2019-03-23 12:53:52 -0600

    landing page

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-let index = require('../controllers/index');
+let get_landing = require('../controllers/landing');
 
 /* GET home page. */
-router.get('/', index.index);
-
+router.get('/', get_landing.get_landing)
 module.exports = router;
